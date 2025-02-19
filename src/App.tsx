a0016769e3e9f25d265e08d5516cdd8021fb1ff6
PwrFr <farrocker@Outlook.co.th>
2025-02-20 01:09:10 +0700

    Add description

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import './App.css';
 
 function App() {
     return (
-        <div className='h-[45dvw] grid place-items-center '>
+        <div className='h-[45dvw] grid place-items-center'>
             <h1 className='text-[20dvw]'>PWRFR</h1>
+            <div>Portfolio</div>
         </div>
     );
 }
