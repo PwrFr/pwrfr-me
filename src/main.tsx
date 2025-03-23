cee2d8743a366ec9f039d4d25dc7541991f85901
PwrFr <farrocker@Outlook.co.th>
2025-03-24 02:08:58 +0700

    refactor: update layout and animations on landing page

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import LadingPage from '@/pages/LadingPage';
 import { BrowserRouter, Routes, Route } from 'react-router';
-import { Layout } from './components/layout/Layout';
+// import { Layout } from './components/layout/Layout';
 import { TablePage } from './pages/Table';
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Layout />}>
+            <Route path='/'>
                 <Route index element={<LadingPage />} />
                 <Route path='table' element={<TablePage />} />
             </Route>
--- src/pages/LadingPage.tsx
+++ src/pages/LadingPage.tsx
@@ -1,21 +1,68 @@
 function App() {
     return (
         <>
-            <div className='h-screen sticky top-0 zoom-name pointer-events-none'>
-                <h1 className='text-[20dvw] font-medium absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%]'>
+            <div className='zoom-name bg-primary z-0'>
+                <h1 className='text-[20dvw] text-background font-medium absolute top-[50%] left-[50%] transform translate-y-[-50%] translate-x-[-50%]'>
                     PWRFR
                 </h1>
             </div>
-            {Array.from({ length: 10 }, (_, i) => (
-                <div
-                    key={i}
-                    className={`h-screen grid fade-card p-24 border-b ${
-                        i % 2 == 0 || 'text-right'
-                    }`}
-                >
-                    <h1 className='text-[10dvw] '>{i + 1}</h1>
+            <div className='relative z-10'>
+                <div className={`h-screen sticky top-0 p-8`}>
+                    <h1 className='text-[8dvw] fade-card'>About Me</h1>
+                    <p className='fade-card'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Repellat, illo? Deleniti maxime nisi suscipit commodi,
+                        ipsum modi laborum sequi cumque, repellat hic quis! Unde
+                        consequatur, obcaecati corrupti voluptates quia non?
+                    </p>
                 </div>
-            ))}
+                <div className={`h-screen`}></div>
+                <div className={`h-screen sticky top-0 bg-background p-8`}>
+                    <h1 className='text-[8dvw]'>Skills</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Molestias, vero? Ut alias sapiente neque maxime
+                        consequuntur aliquid fuga quia cupiditate, sunt delectus
+                        animi aliquam, repellendus maiores, esse quod culpa
+                        porro?
+                    </p>
+                </div>
+                <div
+                    className={`h-screen sticky top-0 bg-primary text-background p-8`}
+                >
+                    <h1 className='text-[8dvw]'>Works</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Asperiores cum dolorum itaque nulla vitae vel quos qui
+                        esse quia! Eveniet nisi sed voluptatem vitae soluta
+                        laudantium, minus natus. Autem, ducimus.
+                    </p>
+                </div>
+                <div
+                    className={`h-screen bg-primary text-background p-8`}
+                ></div>
+            </div>
+            <div className='relative'>
+                <div className={`h-screen sticky top-0 p-8`}>
+                    <h1 className='text-[8dvw]'>About Me</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Repellat, illo? Deleniti maxime nisi suscipit commodi,
+                        ipsum modi laborum sequi cumque, repellat hic quis! Unde
+                        consequatur, obcaecati corrupti voluptates quia non?
+                    </p>
+                </div>
+                <div className={`h-screen sticky top-0 bg-background p-8`}>
+                    <h1 className='text-[8dvw]'>Skills</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Molestias, vero? Ut alias sapiente neque maxime
+                        consequuntur aliquid fuga quia cupiditate, sunt delectus
+                        animi aliquam, repellendus maiores, esse quod culpa
+                        porro?
+                    </p>
+                </div>
+            </div>
         </>
     );
 }
